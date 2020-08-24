008afca501d9de78e10454dbceab5ee1df44427b
Aneesah Quraishi and Gaurav Tyagi <aanconsultancyservicesltd@gmail.com>
2020-08-24 18:48:41 +0100

    Use method delegation with byte buddy

--- src/main/java/com/gt/perf/bb/agent/AgentLoader.java
+++ src/main/java/com/gt/perf/bb/agent/AgentLoader.java
@@ -3,21 +3,42 @@ package com.gt.perf.bb.agent;
 import com.gt.perf.agent.ClassRecorder;
 import net.bytebuddy.agent.builder.AgentBuilder;
 import net.bytebuddy.asm.Advice;
+import net.bytebuddy.description.NamedElement;
+import net.bytebuddy.implementation.MethodDelegation;
+import net.bytebuddy.matcher.ElementMatcher;
 import net.bytebuddy.matcher.ElementMatchers;
 
 import java.lang.instrument.Instrumentation;
+import java.util.Arrays;
+import java.util.Optional;
+
+import static net.bytebuddy.matcher.ElementMatchers.*;
 
 public class AgentLoader {
     public static void premain(String agentArgs, Instrumentation inst) {
+        String packagesToScan = Optional.ofNullable(System.getProperty("packages")).orElse("com");
+        System.out.println("Packages to Scan: " + packagesToScan);
+
+        String[] split = packagesToScan.split(",");
+        ElementMatcher.Junction<NamedElement> matchingPackages = ElementMatchers.nameStartsWith(split[0]);
+
+        for (int i = 1; i < split.length; i++) {
+            System.out.println("Adding " + split[i]);
+            matchingPackages = matchingPackages.and(ElementMatchers.nameStartsWith(split[i]));
+        }
+
         new AgentBuilder.Default()
                 .with(new AgentBuilder.InitializationStrategy.SelfInjection.Eager())
-                //.with(AgentBuilder.Listener.StreamWriting.toSystemError())
-                .type((ElementMatchers.not(ElementMatchers.nameContains("ClassRecorder")))
-                        .and(ElementMatchers.not(ElementMatchers.nameStartsWith("java"))))
+                .with(AgentBuilder.Listener.StreamWriting.toSystemError())
+                .type(not(
+                        nameContains("ClassRecorder")
+                                .or(nameStartsWith("java")))
+                        .and(matchingPackages)
+                )
                 .transform((builder, typeDescription, classLoader, module) -> builder
-                        .method(ElementMatchers.not(ElementMatchers.isConstructor()
-                                .and(ElementMatchers.isStatic())))
-                        .intercept(Advice.to(Interceptor.class))
+                        .method(not(isConstructor()
+                                .and(isStatic())))
+                        .intercept(MethodDelegation.to(TimingInterceptor.class))
                 ).installOn(inst);
 
         Runtime.getRuntime().addShutdownHook(new Thread(ClassRecorder::printReport));
---
+++ src/main/java/com/gt/perf/bb/agent/TimingInterceptor.java
@@ -0,0 +1,24 @@
+package com.gt.perf.bb.agent;
+
+import com.gt.perf.agent.ClassRecorder;
+import net.bytebuddy.implementation.bind.annotation.Origin;
+import net.bytebuddy.implementation.bind.annotation.RuntimeType;
+import net.bytebuddy.implementation.bind.annotation.SuperCall;
+
+import java.lang.reflect.Method;
+import java.util.concurrent.Callable;
+
+public class TimingInterceptor {
+    @RuntimeType
+    public static Object intercept(@Origin Method method,
+                                   @SuperCall Callable<?> callable) throws Exception {
+        long start = System.currentTimeMillis();
+        try {
+            return callable.call();
+        } finally {
+            System.out.println("Adding call duration");
+            ClassRecorder.addCallDuration(method.getName(), System.currentTimeMillis() - start);
+        }
+
+    }
+}
